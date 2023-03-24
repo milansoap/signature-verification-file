fbe7a46f504127beba18da1b3e1dd7bead1e9b24
milansoap <milan.seid123@gmail.com>
2023-03-24 21:09:27 +0100

    inital commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignatureComponent } from './components/signature/signature.component';
+import { VerificationComponent } from './components/verification/verification.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: 'signature',
+    pathMatch: 'full',
+    component: SignatureComponent,
+  },
+  {
+    path: 'verification',
+    pathMatch: 'full',
+    component: VerificationComponent,
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignatureComponent } from './components/signature/signature.component';
+import { VerificationComponent } from './components/verification/verification.component';
+import { SwitcherComponent } from './components/switcher/switcher.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignatureComponent,
+    VerificationComponent,
+    SwitcherComponent
   ],
   imports: [
     BrowserModule,
